4695f22a922291958c3aacf4a5786510a75138c5
juan-saltbox <juan@saltboxmgmt.com>
2025-05-08 18:06:48 -0600

    Inital test

---
+++ .sfdx/typings/lwc/apex/VinController.d.ts
@@ -0,0 +1,6 @@
+declare module "@salesforce/apex/VinController.getCarInformation" {
+  export default function getCarInformation(param: {url: any, vinNumber: any, oppId: any}): Promise<any>;
+}
+declare module "@salesforce/apex/VinController.getOpportunity" {
+  export default function getOpportunity(param: {recordId: any}): Promise<any>;
+}
